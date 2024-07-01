2e07f1692726cd17b9d0438cb78503e2ebbe8f1b
subin5874 <2004489@student.hywoman.ac.kr>
2024-07-01 13:23:28 +0900

    Update Client Folder

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
