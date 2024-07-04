7e3803d2f30682bb1fb444f60d62a621417d69f1
subin5874 <2004489@student.hywoman.ac.kr>
2024-07-04 16:46:08 +0900

    [Feat] MovieList 컴포넌트 추가

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import SearchBar from '../components/SearchBar';
 import ReviewList from '../components/ReviewList';
+import MovieList from '../components/MovieList';
 import styles from './Home.module.css';
 
 function Home() {
@@ -21,6 +22,7 @@ function Home() {
       </div>
       <div className={styles.main_content}>
         <ReviewList />
+        <MovieList />
       </div>
     </div>
   );
