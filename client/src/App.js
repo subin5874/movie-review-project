5136143fdc2003b3ce321339329db5f29e2df6a2
subin5874 <2004489@student.hywoman.ac.kr>
2024-07-04 16:39:31 +0900

    [Feat] React Router를 사용하여 라우팅 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,39 @@
 import './App.css';
-import Home from './pages/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
+import Home from './pages/Home';
+import MovieReview from './pages/MovieReview';
+import MovieSearch from './pages/MovieSearch';
+import MovieDetail from './pages/MovieDetail';
 
 function App() {
+  // API호출
+  /*
+  const fetchMovies = async () => {
+    try {
+      const response = await axios.get(apiEndpoint);
+      setMovies(response.data.results);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setMovies([]); // 에러 발생 시 빈 배열로 초기화
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, [apiEndpoint]); // apiEndpoint가 변경될 때마다 호출
+
+  */
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/movieReview" exact element={<MovieReview />} />
+          <Route path="/movieSearch" exact element={<MovieSearch />} />
+          <Route path="/movieDetailView" exact element={<MovieDetail />} />
         </Routes>
       </BrowserRouter>
     </div>
