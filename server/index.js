011e1c7bae9d44649bffae96459a573ee524903a
subin5874 <2004489@student.hywoman.ac.kr>
2024-07-11 16:28:52 +0900

    [Feat] userRouter 추가

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const db = require('./models');
-const session = require('express-session');
+//const session = require('express-session');
 
 app.use(express.json());
 app.use(cors());
 
 //Routers
+const userRouter = require('./routes/User');
+app.use('/user', userRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3003, () => {
