c56e76eac0d3ecadfcd4d9ed20762d46313ee311
subin5874 <2004489@student.hywoman.ac.kr>
2024-07-16 15:03:53 +0900

    [Refactor] 로그인 상태에 따른 UI 업데이트

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { logoutAsync } from '../store/authSlice';
 
 function Navbar() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  const dispatch = useDispatch();
+
+  const onLogout = () => {
+    dispatch(logoutAsync());
+  };
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
@@ -21,16 +31,32 @@ function Navbar() {
             영화 후기
           </Link>
         </li>
-        <li>
-          <Link to="/" className={styles.nav_link}>
-            로그인
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className={styles.nav_link}>
-            회원가입
-          </Link>
-        </li>
+        {isAuthenticated ? (
+          <li>
+            <Link to="/mypage" className={styles.nav_link}>
+              마이페이지
+            </Link>
+          </li>
+        ) : (
+          <li>
+            <Link to="/login" className={styles.nav_link}>
+              로그인
+            </Link>
+          </li>
+        )}
+        {isAuthenticated ? (
+          <li>
+            <Link onClick={onLogout} className={styles.nav_link}>
+              로그아웃
+            </Link>
+          </li>
+        ) : (
+          <li>
+            <Link to="/signup" className={styles.nav_link}>
+              회원가입
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
