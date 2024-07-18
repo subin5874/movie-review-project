2a8b0932fe9ca864e3f96f03615c179d2a7913f4
subin5874 <2004489@student.hywoman.ac.kr>
2024-07-18 18:29:17 +0900

    [Feat] 검색시 검색 키워드가 MovieSearch로 전달되도록 구현

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SearchBar.module.css';
+import { useLocation } from 'react-router-dom';
 
-function SearchBar() {
-  const searchSubmit = () => {
-    console.log('submit');
+function SearchBar({ onSearch }) {
+  const [searchKeyword, setSearchKeyword] = useState('');
+
+  const InputChange = (e) => {
+    setSearchKeyword(e.target.value);
   };
 
-  const onSearch = () => {
-    console.log('search');
+  const searchSubmit = (e) => {
+    e.preventDefault();
+    console.log('검색어:', searchKeyword);
+    onSearch(searchKeyword);
   };
+
+  const pathName = useLocation().pathname;
+
+  let divClassName;
+  if (pathName === '/') {
+    divClassName = styles.search_container__Home;
+  } else if (pathName === '/movieSearch') {
+    divClassName = styles.search_container__MovieSearch;
+  }
+  //이전 url을 가져와서 Home/MovieSearch에 따라서 className을 다르게 바꾸기
+  //안됨.
 
   return (
-    <div className={styles.search_container}>
+    <div className={divClassName}>
       <span>Search Movie Title</span>
       <form onSubmit={searchSubmit} className={styles.search_form}>
-        <input type="text" className={styles.search_box} onChange={onSearch} />
+        <input
+          type="text"
+          className={styles.search_box}
+          value={searchKeyword}
+          onChange={InputChange}
+        />
         <button type="submit" className={styles.search_btn}>
           <img
             src="/assets/images/search-icon.png"
