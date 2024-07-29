9316f65a099bf3c95e91b80566a5a428814175f5
subin5874 <2004489@student.hywoman.ac.kr>
2024-07-29 23:26:10 +0900

    [Feat] 홈에서 검색 시 영화 검색창으로 이동하도록 변경

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import styles from './SearchBar.module.css';
-import { useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function SearchBar({ onSearch }) {
+  const pathName = useLocation().pathname;
+  const navigate = useNavigate();
+
   const [searchKeyword, setSearchKeyword] = useState('');
 
   const InputChange = (e) => {
@@ -11,11 +14,11 @@ function SearchBar({ onSearch }) {
 
   const searchSubmit = (e) => {
     e.preventDefault();
+    if (pathName === '/') navigate('/movieSearch');
+
     console.log('검색어:', searchKeyword);
     onSearch(searchKeyword);
   };
-
-  const pathName = useLocation().pathname;
 
   let divClassName;
   if (pathName === '/') {
@@ -23,12 +26,14 @@ function SearchBar({ onSearch }) {
   } else if (pathName === '/movieSearch') {
     divClassName = styles.search_container__MovieSearch;
   }
-  //이전 url을 가져와서 Home/MovieSearch에 따라서 className을 다르게 바꾸기
-  //안됨.
 
   return (
     <div className={divClassName}>
-      <span>Search Movie Title</span>
+      {searchKeyword ? (
+        <span> "{searchKeyword}"의 검색결과입니다 </span>
+      ) : (
+        <span>Search Movie Title</span>
+      )}
       <form onSubmit={searchSubmit} className={styles.search_form}>
         <input
           type="text"
