6e837d38ec388b1bf3a089fc33b434516356cdf8
subin5874 <2004489@student.hywoman.ac.kr>
2024-07-30 23:26:44 +0900

    [Feat] 함수에서 movieNo를 매개변수로 받도록 구현

--- client/src/api/movieDetails.js
+++ client/src/api/movieDetails.js
@@ -4,10 +4,10 @@ const apiKey = process.env.REACT_APP_TMDB_API_KEY;
 
 //MID 받아와서 넣어주기
 
-export const getMovieDetails = async () => {
+export const getMovieDetails = async (movieNo) => {
   try {
     const response = await axios.get(
-      `https://api.themoviedb.org/3/movie/${MID}?language=ko-KR`,
+      `https://api.themoviedb.org/3/movie/${movieNo}?language=ko-KR`,
       {
         headers: {
           Authorization: `Bearer ${apiKey}`,
@@ -17,7 +17,7 @@ export const getMovieDetails = async () => {
     );
     console.log('response : ' + JSON.stringify(response.data));
     console.log(response.data.results);
-    return response.data.results;
+    return response.data;
   } catch (error) {
     console.error('Error fetching data:', error);
     return null;
