0e84221414cfd36c53d8b0e6a08d926c8a5f0791
subin5874 <2004489@student.hywoman.ac.kr>
2024-07-30 23:28:22 +0900

    [Refactor] className 및 코드 구조 변경

--- client/src/pages/MovieDetail.js
+++ client/src/pages/MovieDetail.js
@@ -7,13 +7,11 @@ import ReviewList from '../components/ReviewList';
 function MovieDetail() {
   return (
     <div className="main_container">
-      <div className="top_bar">
+      <div className={styles.top_bar}>
         <Navbar />
       </div>
-      <div className="main_content">
+      <div className={styles.main_content}>
         <MovieInfo />
-      </div>
-      <div>
         <ReviewList />
       </div>
     </div>
