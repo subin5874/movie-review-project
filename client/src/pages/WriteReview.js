7526d383c72a42fa5f9ef94e4f8bb044601712ce
subin5874 <2004489@student.hywoman.ac.kr>
2024-07-31 23:38:14 +0900

    [Feat] useNavigate로 전달된 movieNo를 useLocation으로 받아 사용

--- client/src/pages/WriteReview.js
+++ client/src/pages/WriteReview.js
@@ -2,15 +2,19 @@ import React from 'react';
 import styles from './WriteReview.module.css';
 import Navbar from '../components/Navbar';
 import WriteForm from '../components/WriteForm';
+import { useLocation } from 'react-router-dom';
 
 function WriteReview() {
+  const location = useLocation();
+  const movieNo = location.state;
+
   return (
     <div className={styles.writeReview_contaier}>
       <div className={styles.top_bar}>
         <Navbar />
       </div>
       <div className={styles.main_content}>
-        <WriteForm />
+        <WriteForm movieNo={movieNo} />
       </div>
     </div>
   );
