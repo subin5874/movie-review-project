dae516644f045265cbbb9f6c18cdc743281e316b
subin5874 <2004489@student.hywoman.ac.kr>
2024-08-01 23:26:21 +0900

    [Feat] review DB 가져와서 보여주도록 구현

--- client/src/pages/ReviewDetail.js
+++ client/src/pages/ReviewDetail.js
@@ -1,17 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './ReviewDetail.module.css';
 import Navbar from '../components/Navbar';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { formatCreatedAt } from '../utils/formatCreatedAt';
+import { formatRating } from '../utils/formatRating';
 
 function ReviewDetail() {
-  const [reviewDate, setReviewData] = useState({
-    movie_title: '인사이드 아웃2',
-    movie_rating: 5,
-    movie_one_line_review: '정말로 즐거운 영화입니다. 만점!',
-    movie_review:
-      '인사이드 아웃1도 정말 재밌게 봤는데 2는 더 새로운 감정들이 나와서 재밌었어요. 불안이가 제일 공감되는 듯 해요. 아직도 제 안에는 불안이가 크게 자리를 잡고 있는 것 같거든요',
-    user_name: '수달',
-    review_date: '2024.06.21',
-  });
+  const { boardNo } = useParams();
+  console.log(boardNo);
+  const [reviewDate, setReviewData] = useState([]);
+
+  useEffect(() => {
+    let results = [];
+    axios
+      .get('http://localhost:3003/board/reviewDetail/' + boardNo)
+      .then((res) => {
+        results = res.data.reviewDetail;
+        console.log('result' + results);
+        setReviewData(results);
+        setReviewData((reviewDate) => {
+          return {
+            ...reviewDate,
+            createdAt: formatCreatedAt(results.createdAt),
+            rating_score: formatRating(results.Rating.rating_score),
+          };
+        });
+        console.log(reviewDate);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
   const onModifyBtn = () => {
     console.log('수정');
@@ -26,22 +46,22 @@ function ReviewDetail() {
       </div>
       <div className={styles.main_content}>
         <div className={styles.movie_title}>
-          <span>{reviewDate.movie_title}</span>
+          <span>{reviewDate.Movie?.movie_title || '영화 제목'}</span>
         </div>
         <div className={styles.movie_rating}>
-          <span>★★★★</span>
+          <span>{reviewDate.rating_score}</span>
         </div>
         <div className={styles.movie_one_line_review_box}>
           <label>한줄평</label>
-          <span>{reviewDate.movie_one_line_review}</span>
+          <span>{reviewDate.board_one_line_review}</span>
         </div>
         <div className={styles.movie_review}>
-          <span>{reviewDate.movie_review}</span>
+          <span>{reviewDate.board_content}</span>
         </div>
         <div className={styles.review_box_bottom}>
           <div className={styles.user_info_box}>
-            <span>{reviewDate.user_name}</span>
-            <span>{reviewDate.review_date}</span>
+            <span>{reviewDate.User?.user_name || '작성자'}</span>
+            <span>{reviewDate.createdAt}</span>
           </div>
           <div className={styles.modify_btn_box}>
             <button onClick={onModifyBtn} className={styles.modify_btn}>
