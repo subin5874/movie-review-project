88965afe510e30a44def0b7785b89b850092e695
subin5874 <2004489@student.hywoman.ac.kr>
2024-08-01 23:30:31 +0900

    [Feat] movie, board, rating 라우터 추가

--- server/index.js
+++ server/index.js
@@ -10,6 +10,12 @@ app.use(cors());
 //Routers
 const userRouter = require('./routes/User');
 app.use('/user', userRouter);
+const movieRouter = require('./routes/Movie');
+app.use('/movie', movieRouter);
+const boardRouter = require('./routes/Board');
+app.use('/board', boardRouter);
+const ratingRouter = require('./routes/Rating');
+app.use('/rating', ratingRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3003, () => {
