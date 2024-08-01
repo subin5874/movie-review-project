ea97431a806c354185a1c324685fed30f4e652a1
subin5874 <2004489@student.hywoman.ac.kr>
2024-08-01 23:17:52 +0900

    [Refactor] 사용하지 않는 코드 삭제

--- client/src/pages/MovieReview.js
+++ client/src/pages/MovieReview.js
@@ -2,27 +2,14 @@ import React from 'react';
 import styles from './MovieReview.module.css';
 import Navbar from '../components/Navbar';
 import ReviewList from '../components/ReviewList';
-import { useNavigate } from 'react-router-dom';
 
 function MovieReview() {
-  const navigate = useNavigate();
-
-  const writeBtn = () => {
-    navigate('/writeMovieReview');
-  };
   return (
     <div className={styles.main_container}>
       <div className={styles.top_bar}>
         <Navbar />
       </div>
       <div className={styles.main_content}>
-        <button
-          type="button"
-          onClick={writeBtn}
-          className={styles.MovieReview_write_btn}
-        >
-          후기 작성하기
-        </button>
         <ReviewList />
       </div>
     </div>
