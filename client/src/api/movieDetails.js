6c365ebe07f8a158a42903432de62f9a39ea125c
subin5874 <2004489@student.hywoman.ac.kr>
2024-08-01 23:25:20 +0900

    [Refactor] 사용하지 않는 코드 삭제

--- client/src/api/movieDetails.js
+++ client/src/api/movieDetails.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const apiKey = process.env.REACT_APP_TMDB_API_KEY;
 
-//MID 받아와서 넣어주기
-
 export const getMovieDetails = async (movieNo) => {
   try {
     const response = await axios.get(
@@ -15,8 +13,7 @@ export const getMovieDetails = async (movieNo) => {
         },
       }
     );
-    console.log('response : ' + JSON.stringify(response.data));
-    console.log(response.data.results);
+
     return response.data;
   } catch (error) {
     console.error('Error fetching data:', error);
