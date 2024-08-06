765810d85812cc2f46ff5dd75af4e8a9020586a8
subin5874 <2004489@student.hywoman.ac.kr>
2024-08-06 23:22:39 +0900

    [Feat] 별점을 수정하는 엔드포인트 추가

--- server/routes/Rating.js
+++ server/routes/Rating.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Rating } = require('../models/');
+const { where } = require('sequelize');
 
 router.post('/writeRating', async (req, res) => {
   const { rating_score, board_no, movie_no, user_no } = req.body;
@@ -13,6 +14,29 @@ router.post('/writeRating', async (req, res) => {
     });
     res.status(200).json({
       message: 'Rating created successfully',
+      rating_no: writeRatingResult.rating_no,
+    });
+  } catch (err) {
+    console.error('에러:', err);
+  }
+});
+
+router.post('/modifyRating/:boardNo', async (req, res) => {
+  let boardNo = Number(req.params.boardNo);
+  const { rating_score } = req.body;
+  try {
+    const writeRatingResult = await Rating.update(
+      {
+        rating_score: rating_score,
+      },
+      {
+        where: {
+          board_no: boardNo,
+        },
+      }
+    );
+    res.status(200).json({
+      message: 'Rating modify successfully',
       rating_no: writeRatingResult.rating_no,
     });
   } catch (err) {
