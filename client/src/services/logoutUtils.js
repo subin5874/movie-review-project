6165c93dd6048e6a40d912b3104fa4d4ceac4062
subin5874 <2004489@student.hywoman.ac.kr>
2025-05-09 18:20:45 +0900

    [Feat] 로그아웃 로직 분리하여 구현

--- client/src/services/logoutUtils.js
+++ client/src/services/logoutUtils.js
@@ -1,22 +1,13 @@
 import axios from 'axios';
 
-const logoutUser = () => {
-  axios
-    .post(
-      'http://localhost:3003/user/logout',
-      {},
-      {
-        withCredentials: true,
-      }
-    )
-    .then(() => {
-      //dispatch(logoutAsync());
-      localStorage.removeItem('accessToken');
-    })
-    .catch((err) => {
-      console.log('로그아웃 실패');
-      console.log(err);
-    });
+const logoutUser = async () => {
+  try {
+    axios.post('http://localhost:3003/user/logout');
+    localStorage.removeItem('accessToken');
+  } catch (err) {
+    console.log('로그아웃 실패');
+    throw new Error('로그아웃 실패');
+  }
 };
 
 export default logoutUser;
