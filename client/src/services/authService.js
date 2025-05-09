cd3ac7ea6fd64d88fb4d24c4f8d942d0e1b09cd3
subin5874 <2004489@student.hywoman.ac.kr>
2025-05-09 18:21:53 +0900

    [Feat] jwt 검증 및 재발급 로직 구현

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -36,7 +36,7 @@ const useCheckAccessToken = () => {
       } catch (err) {
         if (err.response && err.response.status == 401) {
           console.log(
-            'ToAccessToken이 유효하지 않습니다. RefreshToken을 검증합니다.'
+            'AccessToken이 유효하지 않습니다. RefreshToken을 검증합니다.'
           );
           try {
             const response2 = await axios.post(
@@ -47,7 +47,7 @@ const useCheckAccessToken = () => {
               }
             );
             if (response2 && response2.status == 200) {
-              console.log('RefreshToken이이 유효하여 재발급을 진행합니다.');
+              console.log('RefreshToken이 유효하여 재발급을 진행합니다.');
               if (response2.headers['authorization']) {
                 const accessToken = response2.headers['authorization'].replace(
                   'Bearer ',
@@ -56,19 +56,24 @@ const useCheckAccessToken = () => {
 
                 localStorage.setItem('accessToken', accessToken);
               }
+              console.log('at 재발급을 진행했습니다.');
             }
           } catch (err) {
-            if (err.response2 && err.response2.status == 401) {
+            if (err.response && err.response.status == 401) {
               console.log('RT가 유효하지 않습니다. 로그아웃을 진행합니다.');
-              logoutUser();
-              dispatch(logoutAsync());
+              try {
+                logoutUser();
+                dispatch(logoutAsync());
+              } catch (err) {
+                console.log(err);
+              }
             }
           }
         }
       }
     };
-    //Token 검증 테스트를 위해 60초마다 동작하도록 함
-    const intervalId = setInterval(checkAccessToken, 60 * 1000);
+    //Token 검증 테스트를 위해 --초마다 동작하도록 함
+    const intervalId = setInterval(checkAccessToken, 10 * 1000);
 
     return () => clearInterval(intervalId);
   }, []);
