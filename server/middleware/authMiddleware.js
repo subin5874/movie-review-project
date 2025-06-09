eab6b13eb0958b389fbbbf96011f1ea9d897d36a
subin5874 <2004489@student.hywoman.ac.kr>
2025-06-09 19:35:02 +0900

    [Refactor] 사용하지 않는 코드 삭제

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -6,8 +6,6 @@ function validateAccessToken(req, res, next) {
   const authorizationHeader = req.headers['authorization'];
   try {
     if (!authorizationHeader) {
-      //throw new Error('Authorization 헤더가 없습니다');
-      //throw new Error 를 쓰는게 맞을까?
       return res
         .status(401)
         .json({ message: '로그인이 만료되었습니다. 다시 로그인해주세요.' });
@@ -15,7 +13,6 @@ function validateAccessToken(req, res, next) {
 
     const token = authorizationHeader.split(' ')[1];
     if (!token) {
-      //throw new Error('token이 존재하지 않습니다');
       return res.status(401).json({ message: '토큰이 존재하지 않습니다.' });
     }
 
